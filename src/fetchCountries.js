638563642f81a4cded29418b2a281c30ab7f0dc7
Maryna Dzuh <maryna.dzuh@gmail.com>
2022-11-19 17:06:28 +0200

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,14 +3,14 @@ import Notiflix from 'notiflix';
 function fetchCountries(name) {
     
     
-    const url = ` https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-    return fetch(url).then(response => {
+    const url = ` https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+return fetch(url).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
         return response.json();
     })
-        .then(console.log).catch(error => {
+        .catch(error => {
        Notiflix.Notify.failure('Oops, there is no country with that name');
         });
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
 import {fetchCountries} from './fetchCountries'
 
 const refs = {
@@ -9,24 +8,58 @@ const refs = {
     list: document.querySelector('.country-list'),
     countryCard: document.querySelector('.country-info'),
     
-}
+};
+const DEBOUNCE_DELAY = 300;
 
-refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-
-function onSearch(e) {
+function onSearch() {
     const name = refs.input.value.trim();
     if (name === '') {
-        console.log('noth')
-        return
-    }
-    console.log(name)
+        refs.list.innerHTML = "";
+        refs.countryCard.innerHTML = "";
+        return;
+    };
     fetchCountries(name)
-    
+    .then(data => {
+        if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            return;
+        } else if (data.length === 1) {
+        createMarkupCard(data);
+            return;
+        } else
+        createMarkupList(data)})
 }
-let countriesList = ""
 
 
+function createMarkupCard(arr) {
+    refs.list.innerHTML = ""
+    const markupCard = arr.map(({flags, name, capital, population, languages}) => `<div class= 'country-card'><img src="${flags.svg}" alt="" width='200'>
+<h1 class="title">${name}</h1>
+<div >
+    <div class="field">
+        <h2 class="label">Capital:</h2>
+        <span class="value">${capital}</span>
+    </div>
+    <div class="field">
+        <h2 class="label">Population:</h2>
+        <span class="value">${population}</span>
+    </div>
+    <div class="field">
+        <h2 class="label">Languages:</h2>
+        <span class="value" >${languages.map(({name}) => `<span>${name}</span>`).join(', ') }</span>
+    </div>
+</div></div>`).join('')
+    refs.countryCard.innerHTML = markupCard
 
+}
 
-'italy'
+function createMarkupList(arr) {
+    refs.countryCard.innerHTML = ""
+    const markupList = arr.map(item => `<li class="item">
+        <img src="${item.flags.svg}" alt="" width='50' >
+        <h2 class="title">${item.name}</h2>
+    </li> ` ).join('')
+    refs.list.innerHTML = markupList
+}
